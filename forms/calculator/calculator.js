7bc45f3cb2e43a57fad613b85f676b3adffe14be
Michael-Petrakis <90578378+Michael-Petrakis@users.noreply.github.com>
2021-09-29 16:56:16 -0500

    Version 4

--- forms/calculator/calculator.js
+++ forms/calculator/calculator.js
@@ -1,6 +1,29 @@
+function addTwo(n1, n2) { 
+  let answer = n1 + n2
+  return answer
+  }
 
 btnCalculatorAdd.onclick=function(){
-  let number1 = inptNumber1.value
-  let number2 = inptNumber2.value
-  lblCalculator.value = 
+  let num1 = Number(inptNumber1.value)
+  let num2 = Number(inptNumber2.value)
+  names = inptName.value
+  lblCalculator.value = names + ", the answer is " + addTwo(num1, num2)
+}
+
+function multiplyTwo(n1, n2) { 
+  let answer = n1 * n2
+  return answer
+  }
+
+btnCalculatorMultiply.onclick=function(){
+    let num1 = Number(inptNumber1.value)
+    let num2 = Number(inptNumber2.value)
+    names = inptName.value
+    lblCalculator.value = names + ", the answer is " + multiplyTwo(num1, num2)
+}
+
+btnReset.onclick=function(){
+  lblCalculator.value = ""
+  inptNumber1.value = ""
+  inptNumber2.value = ""
 }
